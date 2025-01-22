dba1c9e4d764bd93c258f3a0eab277ba6221f26b
jdowner944 <jdowner944@gmail.com>
2025-01-22 18:53:50 +0000

    Jan 22, 2025, 1:53 PM

--- js/playPage.js
+++ js/playPage.js
@@ -1,11 +1,30 @@
-var settingsBox = document.getElementById("triviaSettingsBox");
+const settingsBox = document.getElementById("triviaSettingsBox");
 
-triviaSettingsBox.style.display = "none";
+const dim = document.getElementById("overlay");
 
-var settingsButton = document.getElementById("settingsButton");
+const settingsButton = document.getElementById("settingsButton");
 
 settingsButton.addEventListener("click", settingsClick);
 
 function settingsClick() {
-    triviaSettingsBox.style.display = "block";
+    settingsBox.style.display = "block";
+    dim.style.display = "block";
+    settingsBox.style.animation = "zoomIn 0.3s ease-out forwards";
+    
+   /* Code for an off-screen animation; might not be an accessible concept
+   
+   let pos = -250;
+    clearInterval(animateBox);
+
+    function frame () {
+    if (pos === 50) {
+        clearInterval(animateBox);
+    } else {
+        pos++;
+        settingsBox.style.top = pos + 'px';
+    } 
+} 
+
+    animateBox = setInterval(frame, 1); */
+
 }
