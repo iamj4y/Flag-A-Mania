fd630d74936077143fccd4f47156f9459d062566
jdowner944 <jdowner944@gmail.com>
2025-01-22 16:56:23 +0000

    Jan 22, 2025, 11:56 AM

--- js/playPage.js
+++ js/playPage.js
@@ -1,1 +1,9 @@
+var settingsBox = document.getElementById("triviaSettingsBox");
 
+var settingsButton = document.getElementById("settingsButton");
+
+settingsButton.addEventListener("click", settingsClick);
+
+function settingsClick() {
+    settingsButton.innerHTML = "Clicked!";
+}
