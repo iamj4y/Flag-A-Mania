d62fd7ffd430e98ea2c2f7c0f5044f9b025d1136
iamj4y <159561704+iamj4y@users.noreply.github.com>
2025-01-25 17:16:44 -0500

    Update simplePhrases.js

--- js/simplePhrases.js
+++ js/simplePhrases.js
@@ -1,5 +1,19 @@
 function startScript() {
-  
+
+    
+
+    const buttons = document.querySelectorAll('button');
+
+
+    buttons.forEach(button => {
+        button.addEventListener('click', function() {
+          const value = this.value; 
+          document.cookie = "countrySPSelected=" + value + ";" + "path=/"
+
+          window.location.href = '../html/countrySimplePhrases.html';
+        });
+    });
+    
 }
 
 window.addEventListener("load", startScript, false);
