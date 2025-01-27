33b0a6203fbd0393c73c5b964c78516bc5a8c754
iamj4y <159561704+iamj4y@users.noreply.github.com>
2025-01-26 21:29:33 -0500

    Update quizGame.js

--- js/quizGame.js
+++ js/quizGame.js
@@ -1,6 +1,37 @@
 
 
 function mainQuiz() {
+    var countrySet = [
+        {country : "us", name : "United States of America",},
+        {country : "ca", name : "Canada"},
+        {country : "mx", name : "Mexico"},
+        {country : "br", name : "Brazil"},
+        {country : "ar", name : "Argentina"},
+        {country : "co", name : "Columbia"},
+        {country : "pe", name : "Peru"},
+        {country : "cl", name : "Chile"},
+        {country : "cu", name : "Cuba"},
+        {country : "ve", name : "Venezuela"},
+        {country : "cr", name : "Costa Rica"},
+        {country : "ec", name : "Ecuador"},
+        {country : "gt", name : "Guatemala"},
+        {country : "hn", name : "Honduras"},
+        {country : "sv", name : "El Salvador"},
+        {country : "ni", name : "Nicaragua"},
+        {country : "pa", name : "Panama"},
+        {country : "ht", name : "Haiti"},
+        {country : "do", name : "Dominican Republic"},
+        {country : "jm", name : "Jamaica"},
+        {country : "bs", name : "The Bahamas"},
+        {country : "bb", name : "Barbados"},
+        {country : "tt", name : "Trinidad and Tobago"},
+        {country : "gy", name : "Guyana"},
+        {country : "sr", name : "Suriname"},
+        {country : "gf", name : "French Guiana"},
+    ]
+    var cookies = document.cookie.split(';');
+    var gameMode = '';
+    const topSection = document.getElementById('topSection');
 
 }
 
