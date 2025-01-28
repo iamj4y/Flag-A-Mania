9897c910f3ae4e530053ac104267883eb142625e
iamj4y <159561704+iamj4y@users.noreply.github.com>
2025-01-28 12:27:13 -0500

    Update settingsPreGame.js

--- js/settingsPreGame.js
+++ js/settingsPreGame.js
@@ -1,4 +1,9 @@
 function settingsThing(){
+    var timerDur = document.getElementById('timer');
+    var roundAmt = document.getElementById('rounds');
+    var volumeSli = document.getElementById('volumeSlider');
+    var sliderTxt = document.getElementById("sliderTxt");
+    var ttsSwitch = document.getElementById('tts');
     const confirmBtn = document.getElementById("confirmButton");
     const cancelBtn = document.getElementById("cancelButton");
 
@@ -12,6 +17,11 @@ function settingsThing(){
         window.location.href = '../html/play.html';
     }
 
+    sliderTxt.innerHTML = volumeSli.value;
+    function sliderChange() {
+        sliderTxt.innerHTML = this.value;
+    }    
+    volumeSli.addEventListener("input", sliderChange, true);
     confirmBtn.addEventListener("click", clickConfirm);
     cancelBtn.addEventListener("click", clickCancel);
 }
