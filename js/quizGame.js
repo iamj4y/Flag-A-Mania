0f79b65cf4ff2fb76fa38767b48deb3927a54b19
iamj4y <159561704+iamj4y@users.noreply.github.com>
2025-02-23 10:06:37 -0500

    Update quizGame.js

--- js/quizGame.js
+++ js/quizGame.js
@@ -1,41 +1,3 @@
-document.addEventListener("DOMContentLoaded", function () {
-    //darkmode settings
-    const darkModeToggle = document.getElementById("darkMode");
-    const images = [
-        { id: "drawingImage", light : "../img/drawingvecto.png", dark : "../img/StudentStudyingDark.png"},
-        { id: "beingCreativeImage", light : "../img/Being Creative.png", dark : "../img/beingCreativeDark.png"},
-        {id : "aboutUsImage", light : "../img/About Our Team.png", dark : "../img/AboutOurTeamDark.png"},
-    ]
-
-    function updateImages() {
-        images.forEach(imageObj => {
-            let imgElement = document.getElementById(imageObj.id);
-            if (imgElement) {
-                imgElement.src = document.body.classList.contains("dark-mode") ? imageObj.dark : imageObj.light;
-            }
-        })
-    }
-
-    if (localStorage.getItem("darkMode") === "enabled") {
-        document.body.classList.add("dark-mode");
-        darkModeToggle.classList.replace("fa-moon-o", "fa-sun-o");
-        updateImages();
-    }
-
-    darkModeToggle.addEventListener("click", function () {
-        document.body.classList.toggle("dark-mode");
-
-        if (document.body.classList.contains("dark-mode")) {
-            localStorage.setItem("darkMode", "enabled");
-            darkModeToggle.classList.replace("fa-moon-o", "fa-sun-o");
-        
-        } else {
-            localStorage.setItem("darkMode", "disabled");
-            darkModeToggle.classList.replace("fa-sun-o", "fa-moon-o");
-        }
-        updateImages()
-    });
-});
 
 document.addEventListener("DOMContentLoaded", function () {
     //timer
@@ -62,7 +24,7 @@ document.addEventListener("DOMContentLoaded", function () {
             timeLeft.textContent = "0:" + secondsLeft.toString().padStart(2, "0");
         } else {
             clearInterval(interval); // Stop the timer when it reaches 0
-            setTimeout(resetTimer, 1000); // Reset timer after 1 second
+             // Reset timer after 1 second
         }
     }
 
