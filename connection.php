8b26c7437eed150053dcc35a0ec75bf899b85639
WuilmarMM <77976613+WuilmarMM@users.noreply.github.com>
2025-03-13 17:24:29 -0400

    updated db code

--- connection.php
+++ connection.php
@@ -1,8 +1,8 @@
 <?php
 
-$servername = '127.0.0.1';
-$username = 'mdfm1Ku3cO';
-$password = 'lfKaDZ1RkJ';
+$servername = 'localhost';
+$username = 'root';
+$password = '';
 $database = 'dbmdfm1Ku3cO';
 
 $conn = new mysqli($servername, $username, $password, $database);
