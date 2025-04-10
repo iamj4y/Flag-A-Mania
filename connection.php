0d11080ac9f112c517f1d731af5b92165ebccb80
iamj4y <159561704+iamj4y@users.noreply.github.com>
2025-04-10 19:41:14 -0400

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,8 +1,13 @@
 <?php
 
-$servername = 'localhost';
-$username = 'root';
-$password = '';
+//$servername = '127.0.0.1';
+//$username = 'root';
+//$password = '';
+//$database = 'dbmdfm1Ku3cO';
+
+$servername = '127.0.0.1';
+$username = 'mdfm1Ku3cO';
+$password = 'lfKaDZ1RkJ';
 $database = 'dbmdfm1Ku3cO';
 
 $conn = new mysqli($servername, $username, $password, $database);
