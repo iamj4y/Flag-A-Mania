b3bc1582384c037245d0f274fb1c9b12c991b09d
jdowner944 <jdowner944@gmail.com>
2025-04-11 00:01:49 +0000

    Apr 10, 2025, 8:01 PM

--- connection.php
+++ connection.php
@@ -1,9 +1,9 @@
 <?php
 
-$servername = 'localhost';
-$username = 'root';
-$password = '';
-$database = 'dbmdfm1Ku3cO';
+$servername = '127.0.0.1';
+$username = 'mdfm1Ku3cO';
+$password = 'lfKaDZ1RkJ';
+$database = 'dbmdfm1Ku3cO'
 
 $conn = new mysqli($servername, $username, $password, $database);
 
